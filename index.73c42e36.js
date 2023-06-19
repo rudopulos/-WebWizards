f6812912e7ccce429060627151300c89fdd0c1d7
rudopulos <79414851+rudopulos@users.noreply.github.com>
2023-06-19 14:08:10 +0000

    Deploying to gh-pages from @ rudopulos/-WebWizards@a262f8447d7dad3a9d559c38afa015b4134ca5f4 🚀

---
+++ index.73c42e36.js
@@ -0,0 +1,2 @@
+var modal=document.getElementById("read-more-modal"),btn=document.getElementById("read-more-btn"),span=document.getElementsByClassName("modal-how-its-made__close-btn")[0];btn.onclick=function(){modal.style.display="block"},span.onclick=function(){modal.style.display="none"},window.onclick=function(n){n.target==modal&&(modal.style.display="none")};
+//# sourceMappingURL=index.73c42e36.js.map
